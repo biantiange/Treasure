732257714604e572c5e47658ec234eac1dd6e7f4
bigword <37182675+zhao-ning@users.noreply.github.com>
2019-12-09 15:19:00 +0800

    ccc

--- Treasure/MAAndroid/app/src/main/java/com/example/lenovo/maandroid/MainActivity.java
+++ Treasure/MAAndroid/app/src/main/java/com/example/lenovo/maandroid/MainActivity.java
@@ -99,6 +99,7 @@ public class MainActivity extends AppCompatActivity {
         sharedPreferences = getSharedPreferences("parent",MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putString("phoneNumber","15231174460");
+
         editor.putInt("parentId",12);
         editor.commit();
         //---------虚假数据到此结束-------------

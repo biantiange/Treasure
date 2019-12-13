fb00e536a3140e6545e8aab10432528e2e2e8df1
liuzhe0523 <1272991460@qq.com>
2019-12-13 12:00:39 +0800

    1

---
+++ Treasure/MAAndroid/app/src/main/java/com/example/lenovo/maandroid/Entity/AppInfo.java
@@ -0,0 +1,49 @@
+package com.example.lenovo.maandroid.Entity;
+
+import android.graphics.drawable.Drawable;
+
+public class AppInfo {
+    private String appName;
+    private long useTime;
+    private String lastTime;
+    private Drawable applicationIcon;
+
+    public AppInfo(String appName, long useTime, String lastTime, Drawable applicationIcon) {
+        this.appName = appName;
+        this.useTime = useTime;
+        this.lastTime = lastTime;
+        this.applicationIcon = applicationIcon;
+    }
+
+    public String getAppName() {
+        return appName;
+    }
+
+    public void setAppName(String appName) {
+        this.appName = appName;
+    }
+
+    public String getLastTime() {
+        return lastTime;
+    }
+
+    public void setLastTime(String lastTime) {
+        this.lastTime = lastTime;
+    }
+
+    public long getUseTime() {
+        return useTime;
+    }
+
+    public void setUseTime(long useTime) {
+        this.useTime = useTime;
+    }
+    public Drawable getApplicationIcon() {
+        return applicationIcon;
+    }
+
+    public void setApplicationIcon(Drawable applicationIcon) {
+        this.applicationIcon = applicationIcon;
+    }
+
+}

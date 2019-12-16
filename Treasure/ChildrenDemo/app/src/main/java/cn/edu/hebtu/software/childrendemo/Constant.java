6f9fc0edb33b3851fc3fe0c7a083d94c4adffcd9
liuzhe0523 <1272991460@qq.com>
2019-12-16 19:09:18 +0800

    1

--- Treasure/ChildrenDemo/app/src/main/java/cn/edu/hebtu/software/childrendemo/Constant.java
+++ Treasure/ChildrenDemo/app/src/main/java/cn/edu/hebtu/software/childrendemo/Constant.java
@@ -1,6 +1,6 @@
 package cn.edu.hebtu.software.childrendemo;
 
 public class Constant {
-    public static final String BASE_IP="http://10.7.82.161:8080/";
+    public static final String BASE_IP="http://10.7.88.202:8080/";
     public static final String BASE_URL=BASE_IP+"Java/";
 }
